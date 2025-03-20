cfd6be1c64963e315626ca5548366724e55babc5
Joshua Snyder <joshua@posthog.com>
2025-03-20 10:13:45 +0000

    fix: move cloud region prompt location

--- src/nextjs/nextjs-wizard.ts
+++ src/nextjs/nextjs-wizard.ts
@@ -46,7 +46,6 @@ export async function runNextjsWizard(options: WizardOptions): Promise<void> {
 
   const aiConsent = await askForAIConsent();
 
-  const cloudRegion = await askForCloudRegion();
 
   if (!aiConsent) {
     await abort(
@@ -54,6 +53,8 @@ export async function runNextjsWizard(options: WizardOptions): Promise<void> {
       0,
     );
   }
+
+  const cloudRegion = await askForCloudRegion();
 
   const typeScriptDetected = isUsingTypeScript(options);
 
@@ -212,13 +213,12 @@ export async function runNextjsWizard(options: WizardOptions): Promise<void> {
   });
 
   clack.outro(`
-${chalk.green('Successfully installed PostHog!')} ${`\n\n${
-    aiConsent
+${chalk.green('Successfully installed PostHog!')} ${`\n\n${aiConsent
       ? `Note: This uses experimental AI to setup your project. It might have got it wrong, pleaes check!\n`
       : ``
-  }You should validate your setup by (re)starting your dev environment (e.g. ${chalk.cyan(
-    `${packageManagerForOutro.runScriptCommand} dev`,
-  )})`}
+    }You should validate your setup by (re)starting your dev environment (e.g. ${chalk.cyan(
+      `${packageManagerForOutro.runScriptCommand} dev`,
+    )})`}
 
 ${chalk.dim(`If you encounter any issues, let us know here: ${ISSUES_URL}`)}`);
 
@@ -496,8 +496,7 @@ export async function addOrUpdateEnvironmentVariables({
       clack.log.warning(
         `Failed to create ${chalk.bold.cyan(
           relativeEnvFilePath,
-        )} with environment variables for PostHog. Please add them manually. Error: ${
-          error.message
+        )} with environment variables for PostHog. Please add them manually. Error: ${error.message
         }`,
       );
     }
